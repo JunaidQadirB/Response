8be510320b783a5288698587c3fbbe9b3f6a9179
JQ <JeyKeu@users.noreply.github.com>
2015-04-10 00:51:38 +0500

    Update ReturnValue.php

--- src/JeyKeu/Utils/ReturnValue/ReturnValue.php
+++ src/JeyKeu/Utils/ReturnValue/ReturnValue.php
@@ -1,6 +1,6 @@
-<?php namespace JeyKeu\Utils\ReturnValue;
+<?php
 
-/*
+/**
  * ReturnValue is an helper class to handle
  * return values, status codes and messages
  * returned from different functions 
@@ -10,40 +10,37 @@
  * @author   Junaid Qadir <shekhanzai.baloch@gmail.com>
  * @license  http://opensource.org/licenses/GPL-3.0 GPL 3
  * @version  
- * @link     https://gist.github.com/a4f5b9260592134df4e3.git
+ * @link     
  * 
  */
+class ReturnValue {
 
-class ReturnValue
-{
+	const STATUS_OK	 = "OK";
+	const STATUS_ERROR = "ERROR";
 
-    const STATUS_OK    = "OK";
-    const STATUS_ERROR = "ERROR";
+	/**
+	 * 
+	 * @param string $functionName
+	 * @param const $status OK | ERROR
+	 * @param array $returnValue 
+	 * @param string $message 
+	 * @param bool $json false
+	 * @return object | JSON 
+	 */
+	static public function doReturn($functionName, $status = NULL, $returnValue = array(), $message = NULL, $json = false) {
+		if ($status == NULL) {
+			$status = self::STATUS_FAIL;
+		}
+		$return = (object) array(
+					"action"		 => $functionName,
+					"status"		 => $status,
+					"message"		 => $message,
+					"returnValue"	 => $returnValue
+		);
+		if ($json) {
+			$return = json_encode($return);
+		}
+		return $return;
+	}
 
-    /**
-     * 
-     * @param string $functionName
-     * @param const $status
-     * @param array $returnValue 
-     * @param string $message
-     * @param bool $json false
-     * @return object|JSON 
-     */
-    public static function doReturn(
-    $functionName, $status = NULL, $returnValue = array(), $message = NULL, $json = FALSE)
-    {
-        if ($status == NULL) {
-            $status = self::STATUS_ERROR;
-        }
-        $return = (object) array(
-                    "action"      => $functionName,
-                    "status"      => $status,
-                    "message"     => $message,
-                    "returnValue" => $returnValue
-        );
-        if ($json) {
-            $return = json_encode($return);
-        }
-        return $return;
-    }
 }
