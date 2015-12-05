c1ab153b33592ece3a5cd8990e79def097fd69b4
Azi <azibaloch247@gmail.com>
2015-12-05 19:02:29 +0500

    wrote some tests

---
+++ tests/ResponseTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Class ResponseTest
+ */
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_gets_json_string()
+    {
+        $response     = new \Response\Response();
+        $shouldOutPut = json_encode(array('status' => 'success', 'message' => 'Thank You'));
+        $data         = $response->json('success', 'Thank You');
+        $this->assertEquals($shouldOutPut, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_json_string_using_magic_methods()
+    {
+        $response     = new \Response\Response();
+        $shouldOutPut = json_encode(array('status' => 'success', 'message' => 'Thank You'));
+        $data         = $response->writeJson('success', 'Thank You');
+        $this->assertEquals($shouldOutPut, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function it_calls_existing_method_statically()
+    {
+        $shouldOutPut = json_encode(array('status' => 'success', 'message' => 'Thank You'));
+        $data         = \Response\Response::json('success', 'Thank You');
+        $this->assertEquals($shouldOutPut, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function it_accepts_extra_arguments()
+    {
+        $response     = new \Response\Response();
+        $shouldOutPut = json_encode(array(
+            'status'  => 'success',
+            'message' => 'Thank You',
+            'errors'  => array('username' => 'please provide a valid username')
+        ));
+
+        $data = $response->writeJson('success', 'Thank You',
+            array('errors' => array('username' => 'please provide a valid username')));
+        $this->assertEquals($shouldOutPut, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_json_without_message_parameter()
+    {
+        $response     = new \Response\Response();
+        $shouldOutPut = json_encode(array('status' => 'success'));
+        $data         = $response->writeJson('success');
+        $this->assertEquals($shouldOutPut, $data);
+    }
+
+}
