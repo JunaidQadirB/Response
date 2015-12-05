ee4e5e1206c84c39109c989114e82c304bf8c0c7
Azi <azibaloch247@gmail.com>
2015-12-05 19:01:50 +0500

    Updated & added new code

---
+++ src/Contracts/ResponseInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Response\Contracts;
+
+interface ResponseInterface
+{
+    /**
+     * @param $status
+     * @param $message
+     * @param array $args
+     * @return mixed
+     */
+    public function write( $status, $message, $args = array('errors' => array()) );
+}
---
+++ src/Json.php
@@ -0,0 +1,37 @@
+<?php namespace Response;
+
+use Response\Contracts\ResponseInterface;
+
+/**
+ * ReturnValue is an helper class to handle
+ * return values, status codes and messages
+ * returned from different functions
+ *
+ * @category
+ * @package
+ * @author   Junaid Qadir <shekhanzai.baloch@gmail.com>
+ * @license  http://opensource.org/licenses/GPL-3.0 GPL 3
+ * @version
+ * @link
+ */
+class Json implements ResponseInterface
+{
+    /**
+     * @param $status
+     * @param $message
+     * @param array $args
+     * @return mixed
+     */
+    public function write( $status, $message = null, $args = array() )
+    {
+        $data = array(
+            'status' => $status,
+        );
+
+        if ($message) {
+            $data[ 'message' ] = $message;
+        }
+
+        return json_encode(array_merge($data, $args));
+    }
+}
